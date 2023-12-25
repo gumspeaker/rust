8bf524c55a6012444269721250c26ece0a193c9f
xuzhihao.7749 <xuzhihao.7749@bytedance.com>
2023-12-25 13:12:51 +0800

    feat: save

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod sloution;
+mod solution;
 
 fn main() {
-    let res: Vec<i32> = sloution::_1276::num_of_burgers(10, 30);
+    let res: Vec<i32> = solution::_1276::num_of_burgers(10, 30);
     // s1;
 }
